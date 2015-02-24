3117977b38b1658b1c7f3f1591fd53baeb43ec23
jaudhaen <jasper.dhaene@ugent.be>
2015-02-24 14:13:31 +0100

    db stores entries now

--- evenementen/app/controllers/events_controller.rb
+++ evenementen/app/controllers/events_controller.rb
@@ -1,13 +1,49 @@
 class EventsController < ApplicationController
   def index
-    
+     @events = Event.all
+  end
+  
+  def new
+    @event = Event.new
   end
   
   def show
-    
+    @event = Event.find(params[:id])
   end
   
   def create
-    
+    @event = Event.new(params[:event].permit(:title, :text,:begin,:einde))
+    #@event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(params[:event].permit(:title, :text,:begin,:einde))
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path
+  end
+  
+  private
+  def event_params
+     params.require(:event).permit(:title,:text,:startdate,:enddate)
   end
 end
