f5ee3ed8e4a0d22fc90ede8279bcad6353827540
jaudhaen <jasper.dhaene@ugent.be>
2015-02-25 09:49:04 +0100

    cleaned up homepage + added mongol easter egg page

---
+++ evenementen/app/controllers/mongols_controller.rb
@@ -0,0 +1,5 @@
+class MongolsController < ApplicationController
+  def index
+    
+  end
+end
--- evenementen/config/routes.rb
+++ evenementen/config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
   resources :messages
 
   resources :people
+  
+  resources :mongols
 
   root 'homepage#index'
+  
+  get '/mongol' => 'mongols#index'
 
 
 end
