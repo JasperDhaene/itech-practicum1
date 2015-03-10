d88cb98ebf841f19046069c366784d93bfd5617c
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-03-10 14:15:57 +0100

    Added documentation

--- evenementen/app/assets/javascripts/search.js
+++ evenementen/app/assets/javascripts/search.js
@@ -5,6 +5,7 @@
  * 
  * */
 
+// Menu item template
 var template = '' +
 '              <li class="search-item search-item-result">' +
 '                <div class="row">' +
@@ -21,9 +22,9 @@ var template = '' +
 '                </div>' +
 '              </li>';
 
-$(document).ready(function(){
+$(document).on('ready page:load', function(){
   /**
-   * jQuery case-insensitive :contains selector
+   * jQuery case-insensitive ':contains' selector
    * 
    * https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
    * 
@@ -34,13 +35,15 @@ $(document).ready(function(){
       };
   });
 
-/**
- * 
- * Fetch and parse JSON
- * 
- * */
+  /**
+   * 
+   * Fetch, parse and add JSON to the DOM
+   * 
+   * */
   var updateJSON = function(){
+    // Clear previous JSON records
     $('.search-menu-item-result').remove();
+    // Events -> template
     $.getJSON('/events.json', function(data){
       $.each(data.events, function(idx, event){
         $('.navbar-search .search-menu').append(
@@ -51,6 +54,7 @@ $(document).ready(function(){
                                       .replace('{{body}}', event.description));
       });
     });
+    // People -> template
     $.getJSON('/people.json', function(data){
       $.each(data.people, function(idx, person){
         $('.navbar-search .search-menu').append(
@@ -63,12 +67,21 @@ $(document).ready(function(){
     });
   };
 
+  /**
+   * 
+   * Search-and-highlight on input
+   * 
+   * */
   $('.navbar-search .search-bar').on('input', function(){
-    $('.search-menu').show();
     var query = $(this).val();
+
+    // Show menu in case of blur
+    $('.search-menu').show();
+    // Hide all items
     $('.search-item').hide();
+
     if(query.trim() == ''){
-      console.log('Empty');
+      // Show all results on empty string
       $('.search-item-result').each(function(){
         $(this).find('.body-snippet').html($(this).find('.body-hidden').html());
       });
@@ -76,30 +89,43 @@ $(document).ready(function(){
     } else {
       var els = $('.search-item-result:contains(' + query + ')');
       if(els.length == 0){
+        // No results found
         $('.search-item-notfound').show();
       } else $('.search-item-result:contains(' + query + ')').each(function(){
+        // Show all results
         $(this).show();
+
+        // Retrieve full text, cut it to an appropriate string and highlight the matched part
         var fullText = $(this).find('.body-hidden').html();
         var startIdx = fullText.toLowerCase().indexOf(query.toLowerCase());
-        var text = (startIdx == 0 ? '' : '...') +  fullText.substr(Math.max(startIdx - 10, 0), (startIdx - 10 < 0 ? 0 : 10)) + 
+        var text = (startIdx == 0 ? '' : '...') + 
+                            fullText.substr(Math.max(startIdx - 10, 0), (startIdx - 10 < 0 ? 0 : 10)) + 
                             '<strong>' + fullText.substr(startIdx, query.length) + '</strong>' +
                             fullText.substr(startIdx + query.length);
+        // Replace preview/snippet
         $(this).find('.body-snippet').html(text);
       });
     }
   });
 
+  /**
+   * Bind searchbar dropdown events
+   * 
+   * */
   $('.navbar-search .search-bar').on('focus', function(){
     $('.search-menu').show();
   });
 
   $('.navbar-search .search-bar').on('blur', function(){
-    // Blur event fires before click; prevent menu from hiding a little while longer
+    /*
+     * The 'blur' event fires before the 'click' event, so
+     * the menu has to be kept open just a little longer
+     * to allow the user to click on a menu item
+     * */
     setTimeout(function(){
       $('.search-menu').hide();
     }, 100);
   });
-
 
   /**
    * Bootstrap search bar
