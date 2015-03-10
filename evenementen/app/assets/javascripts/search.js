99672491f258153b7fa62875f424fa96b2680aec
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-03-10 12:50:45 +0100

    Added people searching; fixed blur event

--- evenementen/app/assets/javascripts/search.js
+++ evenementen/app/assets/javascripts/search.js
@@ -6,7 +6,7 @@
  * */
 
 var template = '' +
-'              <li class="search-item">' +
+'              <li class="search-item search-item-result">' +
 '                <div class="row">' +
 '                  <div class="col-xs-1">' +
 '                    <i class="glyphicon glyphicon-{{glyphicon}}"></i>' +
@@ -14,14 +14,14 @@ var template = '' +
 '                  <div class="col-xs-10">' +
 '                    <a href="{{link}}">' +
 '                      <strong>{{title}}</strong><br>' +
-'                      <small><span class="ellipsis">{{description}}</span></small>' +
+'                      <small><span class="ellipsis">{{body}}</span></small>' +
 '                    </a>' +
 '                  </div>' +
 '                </div>' +
 '              </li>';
 
 $(document).ready(function(){
-  /*
+  /**
    * jQuery case-insensitive :contains selector
    * 
    * https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
@@ -34,41 +34,60 @@ $(document).ready(function(){
   });
 
 
-
+/**
+ * 
+ * Fetch and parse JSON
+ * 
+ * */
   var updateJSON = function(){
+    $('.search-menu-item-result').remove();
     $.getJSON('/events.json', function(data){
-      $('.navbar-search .search-menu').empty();
       $.each(data.events, function(idx, event){
         // TODO: ellipse text properly
         $('.navbar-search .search-menu').append(
                               template.replace('{{glyphicon}}', 'calendar')
                                       .replace('{{title}}', event.title)
                                       .replace('{{link}}', event.actions.show.href)
-                                      .replace('{{description}}', event.description.substr(0, 60) + '...'));
+                                      .replace('{{body}}', event.description.substr(0, 60) + '...'));
+      });
+    });
+    $.getJSON('/people.json', function(data){
+      $.each(data.people, function(idx, person){
+        $('.navbar-search .search-menu').append(
+                              template.replace('{{glyphicon}}', 'user')
+                                      .replace('{{title}}', person.name)
+                                      .replace('{{link}}', person.actions.show.href)
+                                      .replace('{{body}}', person.email));
       });
     });
   };
 
-  $('.navbar-search .search-bar').val('');
-  updateJSON();
-
   $('.navbar-search .search-bar').on('input', function(){
     $('.search-item').hide();
-    var els = $('.search-item:contains(' + $(this).val() + ')');
+    var els = $('.search-item-result:contains(' + $(this).val() + ')');
     console.log(els.length);
     if(els.length == 0){
-      $('.search-item');
-    }
-    $('.search-item:contains(' + $(this).val() + ')').show();
+      $('.search-item-notfound').show();
+    } else $('.search-item-result:contains(' + $(this).val() + ')').show();
   });
 
-  $('.navbar-search .search-bar').focus(function(){
+  $('.navbar-search .search-bar').on('focus', function(){
     $('.search-menu').show();
   });
-  $('.navbar-search .search-bar').blur(function(){
-    // Ugly hack around blur
-    $(this).delay(10).queue(function(){
+
+  $('.navbar-search .search-bar').on('blur', function(){
+    // Blur event fires before click; prevent menu from hiding a little while longer
+    setTimeout(function(){
       $('.search-menu').hide();
-    });
+    }, 100);
   });
+
+
+  /**
+   * Bootstrap search bar
+   * 
+   * */
+  $('.navbar-search .search-bar').val('');
+  updateJSON();
+  $('.search-item-notfound').hide();
 });
